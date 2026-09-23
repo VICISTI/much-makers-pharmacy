48378075b4b63e926403f910a1606d07847bcc11
udeorji vincente <udeorjivincent2@gmail.com>
2026-09-23 12:01:41 +0100

    Move website to root for GitHub Pages

---
+++ assets/js/medicines.js
@@ -0,0 +1,92 @@
+const medicines = [
+  {
+    id: 1,
+    name: "Levetiracetam 500mg",
+     frontImage: "assets/js/drugs/IMG_014.jpg",
+    backImage: "assets/js/drugs/IMG_001.jpg",
+ 
+  },
+  {
+    id: 2,
+    name: "Oxybutynin 5mg (Oxyprime)",
+    frontImage: "assets/js/drugs/IMG_019.jpg",
+    backImage: "assets/js/drugs/IMG_002.jpg",
+ 
+  },
+  {
+    id: 3,
+    name: "Rivaroxaban 20mg (Rivabax-20)",
+    frontImage: "assets/js/drugs/IMG_008.jpg",
+    backImage: "assets/js/drugs/IMG_009.jpg",
+   
+  },
+  {
+    id: 4,
+    name: "Rivaroxaban 10mg (Rivabax-10)",
+    frontImage: "assets/js/drugs/IMG_010.jpg",
+    backImage: "assets/js/drugs/IMG_011.jpg",
+  
+  },
+  {
+    id: 5,
+    name: "Colchicine 1mg (Colchigout 1)",
+    frontImage: "assets/js/drugs/IMG_012.jpg",
+    backImage: "assets/js/drugs/IMG_013.jpg",
+  },
+  {
+    id: 6,
+    name: "Gliclazide 30mg (Gliclavac 30)",
+    frontImage: "assets/js/drugs/IMG_025.jpg",
+    backImage: "assets/js/drugs/IMG_026.jpg",
+  },
+  {
+    id: 7,
+    name: "Gliclazide 60mg (Gliclavac 60)",
+    frontImage: "assets/js/drugs/IMG_018.jpg",
+    backImage: "assets/js/drugs/IMG_004.jpg",
+
+  },
+  {
+    id: 8,
+    name: "Salbutamol 2.5mg (Salbuvac)",
+    frontImage: "assets/js/drugs/IMG_007.jpg",
+    backImage: "assets/js/drugs/IMG_006.jpg",
+  },
+  {
+    id: 9,
+    name: "Salbutamol 5mg (Salbuvac 5)",
+    frontImage: "assets/js/drugs/IMG_020.jpg",
+    backImage: "assets/js/drugs/IMG_017.jpg",
+  },
+  {
+    id: 10,
+    name: "Ipratropium (Iprahub)",
+    frontImage: "assets/js/drugs/IMG_016.jpg",
+    backImage: "assets/js/drugs/IMG_027.jpg",
+  },
+  {
+    id: 11,
+    name: "Carbimazole 5mg (Carbirose)",
+    frontImage: "assets/js/drugs/IMG_021.jpg",
+    backImage: "assets/js/drugs/IMG_022.jpg",
+  },
+  {
+    id: 12,
+    name: "Pregabalin 75mg (Pregatron)",
+    frontImage: "assets/js/drugs/IMG_024.jpg",
+    backImage: "assets/js/drugs/IMG_023.jpg",
+  },
+  {
+    id: 13,
+    name: "Sodium Hyaluronate 1% (Hussmate-One Plus)",
+    frontImage: "assets/js/drugs/IMG_028.jpg",
+    backImage: "assets/js/drugs/IMG_029.jpg",
+  },
+  {
+    id: 14,
+    name: "Sodium Hyaluronate 1.5% (DuelVisc)",
+    frontImage: "assets/js/drugs/IMG_030.jpg",
+    backImage: "assets/js/drugs/IMG_031.jpg",
+  
+  }
+];
